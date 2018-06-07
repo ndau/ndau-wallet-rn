5443324d973865951843310a634e4a3c79f15518
Kristofer Pelchat <kris.pelchat@oneiro.io>
2018-06-06 22:05:55 -0400

    committing all files as we are in working state with react native navigation

--- android/app/src/main/java/com/ndauwalletrn/MainApplication.java
+++ android/app/src/main/java/com/ndauwalletrn/MainApplication.java
@@ -1,47 +1,28 @@
 package com.ndauwalletrn;
 
-import android.app.Application;
+import android.support.annotation.Nullable;
 
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+public class MainApplication extends NavigationApplication {
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
 
+    @Nullable
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NavigationReactPackage()
-      );
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return null;
     }
 
+    @Nullable
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    public String getJSMainModuleName() {
+        return "index";
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
+
