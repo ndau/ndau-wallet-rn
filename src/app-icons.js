4bb48e17e7195eb25b999c567199be8db886ae9e
Kristofer Pelchat <kris.pelchat@oneiro.io>
2018-06-15 15:36:52 -0400

    added the ability to include svg icons, there is going to be more to this once look and feel gets ironed out, but the infrastructure is there...I have also cleaned up the classes not being used

--- src/app-icons.js
+++ src/app-icons.js
@@ -2,7 +2,6 @@
 // load them once,
 // and use everywhere
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
@@ -13,35 +12,19 @@ const icoMoon = createIconSetFromIcoMoon(icoMoonConfig);
 // here we use active, big, small, very-big..
 const replaceSuffixPattern = /--(active|big|small|very-big)/g;
 const icons = {
-  'ios-person': [ 30, '#bbb' ],
-  'ios-person--big': [ 50, '#bbb' ],
-
-  'ios-person--active': [ 30, '#fff' ],
-  'ios-person--active--big': [ 50, '#fff' ],
-  'ios-person--active--very-big': [ 100, '#fff' ],
-
-  'ios-people': [ 30, '#bbb' ],
-  'ios-people--active': [ 30, '#fff' ],
-
-  'ios-keypad': [ 30, '#bbb' ],
-  'ios-keypad--active': [ 30, '#fff' ],
-
-  'ios-chatbubbles': [ 30, '#bbb' ],
-  'ios-chatbubbles--active': [ 30, '#fff' ],
-
   // Use other Icon provider, see the logic at L39
   facebook: [ 30, '#bbb', FontAwesome ],
   'facebook--active': [ 30, '#fff', FontAwesome ],
   'ndau-icon': [ 30, '#fff', icoMoon ]
 };
 
-const defaultIconProvider = Ionicons;
+const defaultIconProvider = FontAwesome;
 
 let iconsMap = {};
 let iconsLoaded = new Promise((resolve, reject) => {
   new Promise.all(
     Object.keys(icons).map((iconName) => {
-      const Provider = icons[iconName][2] || defaultIconProvider; // Ionicons
+      const Provider = icons[iconName][2] || defaultIconProvider; // FontAwesome
       return Provider.getImageSource(
         iconName.replace(replaceSuffixPattern, ''),
         icons[iconName][0],
--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import { Platform } from 'react-native';
 import { Navigation } from 'react-native-navigation';
-import { registerScreens, registerScreenVisibilityListener } from './screens';
+import { registerScreens } from './screens';
 import { iconsMap, iconsLoaded } from './app-icons';
 
 // screen related book keeping
 registerScreens();
-registerScreenVisibilityListener();
 
 iconsLoaded.then(() => {
   startApp();
@@ -18,29 +17,17 @@ function startApp() {
       screen: 'ndau.Dashboard',
       icon: iconsMap['ndau-icon'],
       title: 'Dashboard'
-    },
-    {
-      label: 'Navigation',
-      screen: 'ndau.Types',
-      icon: require('../img/list.png'),
-      title: 'Navigation Types'
-    },
-    {
-      label: 'Actions',
-      screen: 'example.Actions',
-      icon: require('../img/swap.png'),
-      title: 'Navigation Actions'
     }
   ];
 
-  if (Platform.OS === 'android') {
-    tabs.push({
-      label: 'Transitions',
-      screen: 'example.Transitions',
-      icon: require('../img/transform.png'),
-      title: 'Navigation Transitions'
-    });
-  }
+  // if (Platform.OS === 'android') {
+  //   tabs.push({
+  //     label: 'Transitions',
+  //     screen: 'example.Transitions',
+  //     icon: require('../img/transform.png'),
+  //     title: 'Navigation Transitions'
+  //   });
+  // }
 
   // this will start our app
   Navigation.startTabBasedApp({
@@ -50,7 +37,7 @@ function startApp() {
       tabBarBackgroundColor: '#333333',
       tabBarButtonColor: '#ffffff',
       tabBarSelectedButtonColor: '#ff505c',
-      tabFontFamily: 'BioRhyme-Bold'
+      tabFontFamily: 'TitilliumWeb-Regular'
     },
     appStyle: {
       tabBarBackgroundColor: '#333333',
@@ -62,11 +49,6 @@ function startApp() {
       navBarBackgroundColor: '#333333',
       statusBarColor: '#333333',
       tabFontFamily: 'TitilliumWeb-Regular'
-    },
-    drawer: {
-      left: {
-        screen: 'ndau.Types.Drawer'
-      }
     }
   });
 }
