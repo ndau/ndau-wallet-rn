aff748daca921b45de2022609b1b13d6f99d0889
Kristofer Pelchat <kris.pelchat@oneiro.io>
2018-06-14 23:23:18 -0400

    adding svg icon support

--- android/app/src/main/java/com/ndauwalletrn/MainApplication.java
+++ android/app/src/main/java/com/ndauwalletrn/MainApplication.java
@@ -4,7 +4,9 @@ import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
@@ -16,7 +18,9 @@ public class MainApplication extends NavigationApplication {
     @Nullable
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-        return null;
+        return Arrays.<ReactPackage>asList(
+                new VectorIconsPackage()
+        );
     }
 
     @Nullable
--- src/app.js
+++ src/app.js
@@ -1,66 +1,72 @@
 import { Platform } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens, registerScreenVisibilityListener } from './screens';
-import Image from 'react-native-remote-svg';
+import { iconsMap, iconsLoaded } from './app-icons';
 
 // screen related book keeping
 registerScreens();
 registerScreenVisibilityListener();
 
-const tabs = [
-  {
-    label: 'Dashboard',
-    screen: 'ndau.Dashboard',
-    icon: require('../img/one.png'),
-    title: 'Dashboard'
-  },
-  {
-    label: 'Navigation',
-    screen: 'ndau.Types',
-    icon: require('../img/list.png'),
-    title: 'Navigation Types'
-  },
-  {
-    label: 'Actions',
-    screen: 'example.Actions',
-    icon: require('../img/swap.png'),
-    title: 'Navigation Actions'
-  }
-];
+iconsLoaded.then(() => {
+  startApp();
+});
 
-if (Platform.OS === 'android') {
-  tabs.push({
-    label: 'Transitions',
-    screen: 'example.Transitions',
-    icon: require('../img/transform.png'),
-    title: 'Navigation Transitions'
+function startApp() {
+  const tabs = [
+    {
+      label: 'Dashboard',
+      screen: 'ndau.Dashboard',
+      icon: iconsMap['ndau-icon'],
+      title: 'Dashboard'
+    },
+    {
+      label: 'Navigation',
+      screen: 'ndau.Types',
+      icon: require('../img/list.png'),
+      title: 'Navigation Types'
+    },
+    {
+      label: 'Actions',
+      screen: 'example.Actions',
+      icon: require('../img/swap.png'),
+      title: 'Navigation Actions'
+    }
+  ];
+
+  if (Platform.OS === 'android') {
+    tabs.push({
+      label: 'Transitions',
+      screen: 'example.Transitions',
+      icon: require('../img/transform.png'),
+      title: 'Navigation Transitions'
+    });
+  }
+
+  // this will start our app
+  Navigation.startTabBasedApp({
+    tabs,
+    animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
+    tabsStyle: {
+      tabBarBackgroundColor: '#333333',
+      tabBarButtonColor: '#ffffff',
+      tabBarSelectedButtonColor: '#ff505c',
+      tabFontFamily: 'BioRhyme-Bold'
+    },
+    appStyle: {
+      tabBarBackgroundColor: '#333333',
+      navBarButtonColor: '#ffffff',
+      tabBarButtonColor: '#ffffff',
+      navBarTextColor: '#ffffff',
+      tabBarSelectedButtonColor: '#ff505c',
+      navigationBarColor: '#333333',
+      navBarBackgroundColor: '#333333',
+      statusBarColor: '#333333',
+      tabFontFamily: 'TitilliumWeb-Regular'
+    },
+    drawer: {
+      left: {
+        screen: 'ndau.Types.Drawer'
+      }
+    }
   });
 }
-
-// this will start our app
-Navigation.startTabBasedApp({
-  tabs,
-  animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
-  tabsStyle: {
-    tabBarBackgroundColor: '#333333',
-    tabBarButtonColor: '#ffffff',
-    tabBarSelectedButtonColor: '#ff505c',
-    tabFontFamily: 'BioRhyme-Bold'
-  },
-  appStyle: {
-    tabBarBackgroundColor: '#333333',
-    navBarButtonColor: '#ffffff',
-    tabBarButtonColor: '#ffffff',
-    navBarTextColor: '#ffffff',
-    tabBarSelectedButtonColor: '#ff505c',
-    navigationBarColor: '#333333',
-    navBarBackgroundColor: '#333333',
-    statusBarColor: '#333333',
-    tabFontFamily: 'BioRhyme-Bold'
-  },
-  drawer: {
-    left: {
-      screen: 'ndau.Types.Drawer'
-    }
-  }
-});
