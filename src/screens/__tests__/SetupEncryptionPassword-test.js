5122f8a4e21db705c03536b7566a865c71131cbc
Kristofer Pelchat <kris.pelchat@oneiro.io>
2018-07-05 09:16:33 -0400

    adding the ability to use enzyme with shallow and mount funtionality

--- src/screens/__tests__/SetupEncryptionPassword-test.js
+++ src/screens/__tests__/SetupEncryptionPassword-test.js
@@ -1,3 +1,4 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import SetupEncryptionPassword from '../SetupEncryptionPassword';
@@ -19,10 +20,39 @@ describe('testing SetupEncryptionPassword...', () => {
   });
 
   it('render parentStyles', () => {
-    const wrapper = shallow(<SetupEncryptionPassword parentStyles={styles} />);
-    const render = wrapper.dive();
-    render.find('Button').forEach((child) => {
-      child.simulate('onPress');
-    });
+    const wrapper = mount(<SetupEncryptionPassword parentStyles={styles} />);
+    expect(wrapper.props('parentStyles')).toBeDefined();
+  });
+
+  it('throws an error if click without password', () => {
+    const wrapper = mount(<SetupEncryptionPassword parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert).toBeDefined();
+  });
+
+  it('throws an error if click without confirm password', () => {
+    const wrapper = mount(<SetupEncryptionPassword parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+
+    wrapper.find('TextInput').at(0).instance().value = 'foo';
+
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert).toBeDefined();
+  });
+
+  it('you can proceed after both have been populated', () => {
+    const wrapper = mount(<SetupEncryptionPassword parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+
+    wrapper.find('TextInput').at(0).instance().value = 'foo';
+    wrapper.find('TextInput').at(1).instance().value = 'foo';
+    wrapper.find('CheckBox').at(0).instance().value = true;
+
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert);
   });
 });
--- src/screens/__tests__/SetupUserId-test.js
+++ src/screens/__tests__/SetupUserId-test.js
@@ -1,3 +1,4 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import SetupUserId from '../SetupUserId';
@@ -5,16 +6,44 @@ import SetupUserId from '../SetupUserId';
 import renderer from 'react-test-renderer';
 
 describe('testing SetupUserId...', () => {
+  let styles = StyleSheet.create({
+    wizardText: {
+      color: '#ffffff',
+      fontSize: 20
+    }
+  });
+
   beforeEach(() => {});
   it('renders correctly', () => {
-    var styles = StyleSheet.create({
-      wizardText: {
-        color: '#ffffff',
-        fontSize: 20
-      }
-    });
-
     const tree = renderer.create(<SetupUserId parentStyles={styles} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('throws an error if click without userId', () => {
+    const wrapper = mount(<SetupUserId parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert).toBeDefined();
+  });
+
+  it('throws an error if click without userId', () => {
+    const wrapper = mount(<SetupUserId parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert).toBeDefined();
+  });
+
+  it('does not throw an error if user enters text', () => {
+    const wrapper = mount(<SetupUserId parentStyles={styles} />);
+    const onlyButton = wrapper.find('Button').at(0);
+
+    wrapper.find('TextInput').at(0).instance().value = 'foo';
+    // expect(wrapper.state('userId')).toEqual('foo');
+
+    onlyButton.simulate('click');
+    const alert = wrapper.find('Alert').at(0);
+    expect(alert);
   });
 });
