f7ee2d9909c07a9c75f82f8d9e1f593807a322a3
Josh <fetmar@gmail.com>
2018-07-27 16:38:24 -0400

    mock securerandom with Math.random()

--- __mocks__/react-native-securerandom.js
+++ __mocks__/react-native-securerandom.js
@@ -3,9 +3,9 @@
 module.exports = {
     generateSecureRandom: jest.fn(() => {
         return new Promise((resolve) => {
-            const a = new Uint8Array(1);
-            a[0] = 4; // chosen by fair dice roll. guaranteed to be random. xkcd.com/221
-            resolve(a);
+            resolve(Uint8Array.from([0, 0, 0, 0, 0, 0, 0, 0].map(
+                () => Math.floor(Math.random() * 256)
+            )));
         })
     })
 }
