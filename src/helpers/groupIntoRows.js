236c26f92d6e5a97816688c7d6438a727f980548
Josh <fetmar@gmail.com>
2018-08-06 11:45:24 -0400

    no more copying and pasting for me

--- src/helpers/groupIntoRows.js
+++ src/helpers/groupIntoRows.js
@@ -1,7 +1,7 @@
 export default function (arr, length) {
     let res = [];
-    for (let i = 0; i < this.props.shuffledWords.length; i += ROW_LENGTH) {
-        res.push(this.props.shuffledWords.slice(i, i + ROW_LENGTH));
+    for (let i = 0; i < arr.length; i += ROW_LENGTH) {
+        res.push(arr.slice(i, i + ROW_LENGTH));
     }
     return res;
 }
