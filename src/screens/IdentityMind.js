c17ebd38dea2a825cda86adbd3da97c36495798e
Kristofer Pelchat <kristoferpelchat@Kristofers-Mac-mini.local>
2019-01-10 23:36:46 -0500

    got iOS working by adding a userAgent for the phone

--- src/screens/IdentityMind.js
+++ src/screens/IdentityMind.js
@@ -46,6 +46,7 @@ class IdentityMind extends Component {
             onLoadProgress={e => console.log(e.nativeEvent.progress)}
             onMessage={this.handleMessage}
             bounces={false}
+            userAgent='Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
           />
         </View>
       </SafeAreaView>
