ffacc969d11ff04e864ed580a1a83c61b1d63521
Josh <fetmar@gmail.com>
2019-04-26 13:12:52 -0400

    cache and lazy refresh node list from s3

--- src/api/ServiceDiscovery.js
+++ src/api/ServiceDiscovery.js
@@ -2,6 +2,7 @@ import ServiceDiscoveryError from '../errors/ServiceDiscoveryError'
 import AsyncStorageHelper from '../model/AsyncStorageHelper'
 import APICommunicationHelper from '../helpers/APICommunicationHelper'
 import LoggingService from '../services/LoggingService'
+import moment from 'moment'
 
 const AWS_S3_SERVICE_JSON_PROD =
   'https://s3.us-east-2.amazonaws.com/ndau-json/services-prod.json'
@@ -10,6 +11,12 @@ const AWS_S3_SERVICE_JSON_TEST =
 const AWS_S3_SERVICE_JSON_DEV =
   'https://s3.us-east-2.amazonaws.com/ndau-json/services-dev.json'
 
+const cache = {
+  lastChecked: moment(0), // moment date. starts a 0 to immediately invalidate the cache
+  nodes: []
+}
+
+const CACHE_TTL = 1000 * 60 * 5 // 1000 * 60 * 5 = five minutes
 const getServiceNodeURL = async () => {
   let url = AWS_S3_SERVICE_JSON_PROD
   if ((await AsyncStorageHelper.isTestNet()) || __DEV__) {
@@ -18,11 +25,14 @@ const getServiceNodeURL = async () => {
   LoggingService.debug(`Service Discovery URL: ${url}`)
 
   try {
-    const response = await APICommunicationHelper.get(url)
-    const apinodes = response.apinodes
+    if ( moment().diff(cache.lastChecked) > CACHE_TTL ) {
+      const response = await APICommunicationHelper.get(url)
+      cache.nodes = response.apinodes
+      cache.lastChecked = moment()
+    }
 
     // return a random service for use
-    return apinodes[Math.floor(Math.random() * apinodes.length)]
+    return cache.nodes[Math.floor(Math.random() * cache.nodes.length)]
   } catch (error) {
     LoggingService.debug(error)
     throw new ServiceDiscoveryError()
