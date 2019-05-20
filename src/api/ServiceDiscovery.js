29b754ce0b536f5f08c8848769c5860762fd6132
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-05-20 12:14:41 -0400

    use devenet instead of testnet when __DEV__

--- src/api/ServiceDiscovery.js
+++ src/api/ServiceDiscovery.js
@@ -75,7 +75,7 @@ const _parseServicesForNodes = async (serviceDiscovery, type) => {
   let environment = await AsyncStorageHelper.getNetwork()
   // if we are in simulators then force to testnet
   if (__DEV__) {
-    await AsyncStorageHelper.useTestNet()
+    await AsyncStorageHelper.useDevNet()
     environment = await AsyncStorageHelper.getNetwork()
   }
 
