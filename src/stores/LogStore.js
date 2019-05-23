a54e277c9f24216263f9b2a29b7c5721218d0c33
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-05-23 10:51:41 -0400

    kp-contactsupportrework

--- src/stores/LogStore.js
+++ src/stores/LogStore.js
@@ -11,7 +11,26 @@ class LogStore {
     return LogStore.instance
   }
 
+  _scrubData = (...data) => {
+    if (!data) return data
+    let stringData = data
+      .map(e => {
+        const t = typeof e
+        if (t === 'string' || t === 'number') {
+          return e.toString()
+        } else {
+          return JSON.stringify(e)
+        }
+      })
+      .join(',')
+    // pull out ALL private keys
+    let scrubbedData = stringData.replace(/\b"npvt[^"]+"\b/g, '"a"')
+    return scrubbedData
+  }
+
   log (logData) {
+    logData = this._scrubData(logData)
+
     if (__DEV__) console.log(logData)
 
     if (this._logData.length === AppConfig.MAX_LOG_ENTRIES) {
--- src/stores/__tests__/LogStore-test.js
+++ src/stores/__tests__/LogStore-test.js
@@ -18,3 +18,37 @@ test('Make sure we do not go past the max', async () => {
   expect(Object.keys(data).length).toBe(100)
   expect(Object.keys(data)[0].entry === 'entry for 349')
 })
+
+test('Make sure we DO NOT show private keys if they are present', async () => {
+  LogStore.log(
+    'testing 1..."npvt8ard395saaaaafnu25p694rkaxkir29ux5quru9b6nq4m3au4gugm2riue5xuqyyeabkkdcz9mc688665xmidzkjbfrw628y7c5zit8vcz6x7hjuxgfeu4kqaqxb"'
+  )
+  LogStore.log(
+    'testing "npvt8ard395saaaaafnu25p694rkaxkir29ux5quru9b6nq4m3au4gugm2riue5xuqyyeabkkdcz9mc688665xmidzkjbfrw628y7c5zit8vcz6x7hjuxgfeu4kqaqxb" and public "npubaard3952aaaaaetmg8gtxb6g75n9i3fxi8y3465qgjb7mmfv47nupz5kgettw7tpkazt5utca85h8ri4qquegqs8byaqhwx66uhnxx8xz4dqfzbgavvs4jkbj44b"'
+  )
+
+  const data = LogStore.getLoggingData()
+  // because above we added LOTS
+  expect(Object.keys(data).length).toBe(100)
+  expect(
+    Object.keys(data)[0].entry ===
+      'testing "a" and public "npubaard3952aaaaaetmg8gtxb6g75n9i3fxi8y3465qgjb7mmfv47nupz5kgettw7tpkazt5utca85h8ri4qquegqs8byaqhwx66uhnxx8xz4dqfzbgavvs4jkbj44b"'
+  )
+})
+
+test('Make sure we DO NOT show private keys and careful to not remove npvt somwhere in teh string needlessly', async () => {
+  LogStore.log(
+    'testing 1..."npvt8ard395saaaaafnu25p694rkaxkir29ux5quru9b6nq4m3au4gugm2riue5xuqyyeabkkdcz9mc688665xmidzkjbfrw628y7c5zit8vcz6x7hjuxgfeu4kqaqxb"'
+  )
+  LogStore.log(
+    'testing "npvt" npvt "npvt8ard395saaaaafnu25p694rkaxkir29ux5quru9b6nq4m3au4gugm2riue5xuqyyeabkkdcz9mc688665xmidzkjbfrw628y7c5zit8vcz6x7hjuxgfeu4kqaqxb" and public "npubaard3952aaaaaetmg8gtxb6g75n9i3fxi8y3465qgjb7mmfv47nupz5kgettw7tpkazt5utca85h8ri4qquegqs8byaqhwx66uhnxx8xz4dqfzbgavvs4jkbj44b"'
+  )
+
+  const data = LogStore.getLoggingData()
+  // because above we added LOTS
+  expect(Object.keys(data).length).toBe(100)
+  expect(
+    Object.keys(data)[0].entry ===
+      'testing "npvt" npvt "a" and public "npubaard3952aaaaaetmg8gtxb6g75n9i3fxi8y3465qgjb7mmfv47nupz5kgettw7tpkazt5utca85h8ri4qquegqs8byaqhwx66uhnxx8xz4dqfzbgavvs4jkbj44b"'
+  )
+})
