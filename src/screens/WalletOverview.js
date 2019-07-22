6817a10e2ca1e5d2e565bd9dbe6c3c420d3b67f6
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-07-22 16:02:24 -0400

    There is no need for a promise in the loads as those are done with events. Also, previously I wasn't using the hash when I accessed the wallet in the user object. I now do so refresh updates correctly

--- src/screens/WalletOverview.js
+++ src/screens/WalletOverview.js
@@ -65,10 +65,7 @@ class WalletOverview extends Component {
     if (this.props.navigation.getParam('refresh')) {
       await this._onRefresh()
     } else {
-      const wallet = await this._loadMetricsAndSetState(WalletStore.getWallet())
-      if (wallet) {
-        WalletStore.setWallet(wallet)
-      }
+      await this._loadMetricsAndSetState(WalletStore.getWallet())
     }
 
     const error = this.props.navigation.getParam('error', null)
