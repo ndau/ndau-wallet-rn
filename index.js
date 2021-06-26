05bcc50868eafd9b67545be166b10d6eb842c2d1
John Greenland <jsgjnr@yahoo.com>
2021-06-26 11:22:17 -0400

    jsg exp

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ import { AppRegistry } from 'react-native'
 import App from './src/app'
 import LogStore from './src/stores/LogStore'
 
-if (__DEV__) {
-  import('./ReactotronConfig').then(() => LogStore.log('Reactotron Configured'))
-}
+// if (__DEV__) {
+//   import('./ReactotronConfig').then(() => LogStore.log('Reactotron Configured'))
+// }
 
 AppRegistry.registerComponent('ndauwalletrn', () => App)
